23da77fedc807fa3d15093418b8b7fc4352860b5
anna <g.molybog@levi9.com>
2021-09-12 22:31:18 +0200

    HW04: optimize code, choose optimal heap size

---
+++ hw04-gc/src/main/java/calculator/Data.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Data {
+    private int value = 0;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
